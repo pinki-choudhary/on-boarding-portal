67275a3c64a8170e50e4151be07f7c3127f3a46c
Pinki Choudhary <pinki.choudhary@nagarro.com>
2019-04-20 13:37:42 +0530

    extra src folder removed

--- on-boarding-portal/src/app/src/app/shared/custom-material.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomMaterialPipe } from './custom-material.pipe';
-
-describe('CustomMaterialPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CustomMaterialPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- on-boarding-portal/src/app/src/app/shared/custom-material/category-wise-student.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryWiseStudentPipe } from './category-wise-student.pipe';
-
-describe('CategoryWiseStudentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryWiseStudentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
